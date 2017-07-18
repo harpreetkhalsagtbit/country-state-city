4a30a6211493b86006dbad454313923e964719b0
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2017-07-18 13:03:40 +0530

    Fix: return valules were missing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var countryList = require("./lib/country.json")
 var stateList = require("./lib/state.json")
 var cityList = require("./lib/city.json")
 
-
+console.log(countryList)
 module.exports = {
 	getCountryById: function(id) {
 		if(!isNaN(+(id)))
@@ -26,11 +26,13 @@ module.exports = {
 		var states = statelist.filter(function(value, index) {
 			return value.country_id == countryId
 		})
+		return states
 	},
 	getCitiesOfState: function(stateId) {
 		var cities = citylist.filter(function(value, index) {
 			return value.state_id == stateId
 		})
+		return cities
 	},
 	getAllCountries: function(stateId) {
 		return countryList;
