c95b1055b5b4f6a650ae9790e2353a3738e5b3eb
Harpreet Singh <harpreetkhalsagtbit@gmail.com>
2019-01-18 14:44:45 +0530

    sort before returning code - New state or cities has been added and to leep previous indices as it is we have added new state and cities as last items in the arrays and has marked them with correposnding incremented ids which keeps them away from other siblings and are also not in alphabetical order, so we are sorting them agian after filter

--- index.js
+++ index.js
@@ -3,39 +3,47 @@ var stateList = require("./lib/state.json")
 var cityList = require("./lib/city.json")
 
 var country_state_city = {
-	getCountryById: function(id) {
+	getCountryById: function (id) {
 		return _findEntry(countryList, id);
 	},
-	getStateById: function(id) {
+	getStateById: function (id) {
 		return _findEntry(stateList, id);
 	},
-	getCityById: function(id) {
+	getCityById: function (id) {
 		return _findEntry(cityList, id);
 	},
-	getStatesOfCountry: function(countryId) {
-		var states = stateList.filter(function(value, index) {
+	getStatesOfCountry: function (countryId) {
+		var states = stateList.filter(function (value, index) {
 			return value.country_id == countryId
 		})
-		return states
+		return states.sort(compare)
 	},
-	getCitiesOfState: function(stateId) {
-		var cities = cityList.filter(function(value, index) {
+	getCitiesOfState: function (stateId) {
+		var cities = cityList.filter(function (value, index) {
 			return value.state_id == stateId
 		})
-		return cities
+		return cities.sort(compare)
 	},
-	getAllCountries: function() {
+	getAllCountries: function () {
 		return countryList;
 	}
 
 }
 
 let _findEntry = (source, id) => {
-	if(!isNaN(id) && source != null) {
+	if (!isNaN(id) && source != null) {
 		let idx = source.findIndex((c, i) => c.id === id);
 		return (idx !== -1) ? source[idx] : "";
 	}
 	else return "";
+}
+
+function compare(a, b) {
+	if (a.name < b.name)
+		return -1;
+	if (a.name > b.name)
+		return 1;
+	return 0;
 }
 
 module.exports = country_state_city;
