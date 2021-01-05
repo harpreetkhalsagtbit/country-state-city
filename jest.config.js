24a8021cc57ebb76bfa260960034f0861eb9fcb1
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2021-01-05 17:23:08 +0530

    ESLnt, formatting and updated read me

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,10 @@
 module.exports = {
-    "roots": [
-      "<rootDir>/"
-    ],
-    moduleFileExtensions: [
-      'js',
-      'ts',
-      'tsx',
-      'json',
-      'node',
-    ],
-    testMatch: [
-      '**/*.test.ts',
-      '**/*.test.js'
-    ],
-    testPathIgnorePatterns: [
-      '/node_modules/',
-      'dist'
-    ],
-    collectCoverage: true,
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-  }
+	roots: ['<rootDir>/'],
+	moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
+	testMatch: ['**/*.test.ts', '**/*.test.js'],
+	testPathIgnorePatterns: ['/node_modules/', 'dist'],
+	collectCoverage: true,
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+};
