dbd0ac6acdda5f97bbe02ab1dc8a087ccee355ed
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2021-01-05 16:19:37 +0530

    Eslnt setup and fixes

--- index.ts
+++ index.ts
@@ -1,57 +1,58 @@
 import countryList from './lib/country.json';
 import stateList from './lib/state.json';
 import cityList from './lib/city.json';
-import { ICountry, ICity, IState} from './src/interface';
-export { ICountry, ICity, IState} from './src/interface';
+import { ICountry, ICity, IState } from './src/interface';
 
- export default {
-    getStatesOfCountry: function (countryCode: string): IState[] {
-        var states = stateList.filter(function (value) {
-            return value.countryCode === countryCode
-        })
-        return states.sort(compare)
-    },
-    getCitiesOfState: function (countryCode: string, stateCode: string): ICity[] {
-        var cities = cityList.filter(function (value) {
-            return (value.countryCode === countryCode && value.stateCode === stateCode)
-        })
-        return cities.sort(compare)
-    },
-    getCitiesOfCountry: function (countryCode: string): ICity[] {
-        var cities = cityList.filter(function (value) {
-            return (value.countryCode === countryCode)
-        })
-        return cities.sort(compare)
-    },
-    getAllCountries: function (): ICountry[] {
-        return countryList;
-    },
-    getAllStates: function (): IState[] {
-        return stateList;
-    },
-    getAllCities: function (): ICity[] {
-        return cityList;
-    },
-    getCountryByCode: function (isoCode: string): ICountry {
-        return _findEntryByCode(countryList, isoCode);
-    },
-    getStateByCode: function (isoCode: string): IState {
-        return _findEntryByCode(stateList, isoCode);
-    }
-}
+export { ICountry, ICity, IState } from './src/interface';
 
-let _findEntryByCode = (source: any, code: string) => {
-    if (code && source != null) {
-        let codex = source.findIndex((c:any) => c.isoCode === code);
-        return (codex !== -1) ? source[codex] : "";
-    }
-    else return "";
-}
+const _findEntryByCode = (source: any, code: string) => {
+	if (code && source != null) {
+		const codex = source.findIndex((c: any) => {
+			return c.isoCode === code;
+		});
+		return codex !== -1 ? source[codex] : '';
+	}
+	return '';
+};
 
-function compare(a:any, b:any) {
-    if (a.name < b.name)
-        return -1;
-    if (a.name > b.name)
-        return 1;
-    return 0;
-}
+const compare = (a: any, b: any) => {
+	if (a.name < b.name) return -1;
+	if (a.name > b.name) return 1;
+	return 0;
+};
+
+export default {
+	getStatesOfCountry(countryCode: string): IState[] {
+		const states = stateList.filter((value) => {
+			return value.countryCode === countryCode;
+		});
+		return states.sort(compare);
+	},
+	getCitiesOfState(countryCode: string, stateCode: string): ICity[] {
+		const cities = cityList.filter((value: { countryCode: string; stateCode: string; }) => {
+			return value.countryCode === countryCode && value.stateCode === stateCode;
+		});
+		return cities.sort(compare);
+	},
+	getCitiesOfCountry(countryCode: string): ICity[] {
+		const cities = cityList.filter((value: { countryCode: string; }) => {
+			return value.countryCode === countryCode;
+		});
+		return cities.sort(compare);
+	},
+	getAllCountries(): ICountry[] {
+		return countryList;
+	},
+	getAllStates(): IState[] {
+		return stateList;
+	},
+	getAllCities(): ICity[] {
+		return cityList;
+	},
+	getCountryByCode(isoCode: string): ICountry {
+		return _findEntryByCode(countryList, isoCode);
+	},
+	getStateByCode(isoCode: string): IState {
+		return _findEntryByCode(stateList, isoCode);
+	},
+};
