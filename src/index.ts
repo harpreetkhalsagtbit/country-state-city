e445d108a710fc50ef8dd3e8aa33946c9a18033f
honia19 <honchenko@gmail.com>
2022-07-14 11:23:32 +0100

    fix: remove Timezone interface from export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Country from './country';
 import State from './state';
 import City from './city';
-import { ICountry, IState, ICity, Timezones } from './interface';
+import { ICountry, IState, ICity } from './interface';
 
 export { Country };
 export { State };
 export { City };
-export { ICountry, IState, ICity, Timezones };
+export { ICountry, IState, ICity };
