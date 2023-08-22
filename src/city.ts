6a9de1e9511bc1420754d2566bc6c911ce6ae497
Nisharg Shah <nishargshah3101@gmail.com>
2023-08-22 23:20:47 +0530

    fix: getAllCities function keys param converted to optional

--- src/city.ts
+++ src/city.ts
@@ -12,13 +12,13 @@ const KEYS = [
 
 let convertedCityList: ICity[] = [];
 // Get a list of all cities.
-function getAllCities(keys: string[]): ICity[] {
+function getAllCities(keys?: string[] = KEYS): ICity[] {
 	if (convertedCityList.length) {
 		return convertedCityList;
 	}
 
 	const cityJSON: string[][] = cityList;
-	convertedCityList = convertArrayToObject(keys, cityJSON);
+	convertedCityList = convertArrayToObject(keys ?? KEYS, cityJSON);
 	return (convertedCityList as unknown as ICity[])
 }
 
@@ -27,7 +27,7 @@ function getCitiesOfState(countryCode: string, stateCode: string): ICity[] {
 	if (!stateCode) return [];
 	if (!countryCode) return [];
 
-	const cityList = getAllCities(KEYS);
+	const cityList = getAllCities();
 	const cities = (cityList as ICity[]).filter((value: { countryCode: string; stateCode: string }) => {
 		return value.countryCode === countryCode && value.stateCode === stateCode;
 	});
@@ -39,7 +39,7 @@ function getCitiesOfState(countryCode: string, stateCode: string): ICity[] {
 function getCitiesOfCountry(countryCode: string): ICity[] | undefined {
 	if (!countryCode) return [];
 
-	const cityList = getAllCities(KEYS);
+	const cityList = getAllCities();
 	const cities = (cityList as ICity[]).filter((value: { countryCode: string }) => {
 		return value.countryCode === countryCode;
 	});
